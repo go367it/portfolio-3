0ff6dce86bda2bfdf941cd9c05c3174107e82b05
go367it <satyamkumar3001@gmail.com>
2020-12-03 00:54:12 +0530

    updated code

--- demo3/src/Pages/Home-page/Homepage.js
+++ demo3/src/Pages/Home-page/Homepage.js
@@ -1,27 +1,37 @@
 import React, { Component } from 'react';
 import NavBar from '../Navbar/Navbar';
+import anime from 'animejs';
+import './Home-page.css';
+import { motion } from 'framer-motion';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 class HomePage extends Component{
     render(){
+        const animation = anime({
+            targets: '.box',
+            translateX: 250
+          });
         return(
             <div className="Hero-section">
                 <NavBar />
                 <div className="hero-section">
                     <div className="container">
                         <div className="row">
-                            <div className="left-section">
-                                <div className="container">
-                                    <div className="col-md-6">
-                                        <h1>Welcome to this website</h1>
+                            <div className="col-md-4"></div>
+                            <motion.div animate={{x: 0,
+                                        y: 200,
+                                        scale: 4,
+                                        rotate: 160,}}>
+                                <div className="col-md-4">
+                                    <div className="box">
                                     </div>
                                 </div>
-                            </div>
-                            <div className="right-section">
-                                <div className="container">
-                                    <div className="col-md-6">
-                                        <h1>Welcome to this website</h1>
-                                    </div>
-                                </div>
+                            </motion.div>
+                            <div className="col-md-4">
+                            <Button variant="contained" color="primary">
+                                    Primary
+                                    </Button>
                             </div>
                         </div>
                     </div>
